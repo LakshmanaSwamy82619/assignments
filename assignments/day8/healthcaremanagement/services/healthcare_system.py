a8f8c724256f7e657c9b312d9d020d4d614e9a80
LakshmanaSwamy82619 <viswanadham.l@prodapt.com>
2026-07-15 15:45:36 +0530

    Delete assignments/day8/healthcaremanagement/services/healthcare_system.py

--- assignments/day8/healthcaremanagement/services/healthcare_system.py
+++
@@ -1,9 +0,0 @@
-from services.patient_service import PatientService
-from services.doctor_service import DoctorService
-from services.appointment_service import AppointmentService
-
-class HealthcareSystem:
-    def __init__(self, datastore):
-        self.patient_service = PatientService(datastore)
-        self.doctor_service = DoctorService(datastore)
-        self.appointment_service = AppointmentService(datastore)
